069c94fce140e1aa076516cd77ea6cc10f056475
KingProsper95 <ajoninajerryprosper@gmail.com>
2024-08-29 09:54:03 +0100

    project structure restructured

---
+++ CamtelSupplyAPI/serializers/productSerializer.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from CamtelSupplyAPI.models import Category, Product
+from CamtelSupplyAPI.serializers.categorySerializer import CategorySerializer
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field = 'pk'
+    )
+    class Meta:
+        model = Product
+        fields = ['url', 'id', 'name', 'category', 'created_at']
+    
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category_name = category_data.get('name')
+
+        # Try to get the category if it exists, otherwise create a new one
+        category, created = Category.objects.get_or_create(
+            name__iexact=category_name,
+            defaults=category_data
+        )
+
+        # Now create the product with the existing or new category
+        product = Product.objects.create(category=category, **validated_data)
+
+        return product
+    
+    def update(self, instance, validated_data):
+        category_name = validated_data.pop('category').get('name')
+        instance.name = validated_data.get('name')
+
+        #check if the category with the given name is present in the database
+        query = Category.objects.filter(name__iexact=category_name)
+        if query.exists():
+            instance.category = query.first()
+            instance.save()
+        else:
+            raise serializers.ValidationError(
+            f'\'{category_name}\' not found'
+            )
+        return instance
---
+++ CamtelSupplyAPI/viewss.py
@@ -0,0 +1,45 @@
+# from django.shortcuts import render
+# from rest_framework.response import Response
+# from rest_framework.request import Request
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.views import APIView
+# from rest_framework import generics
+# from .models import Category, Product, Supplier, Entity
+# from .serializers import CategorySerializer, ProductSerializer, SupplierSerializer, EntitySerializer
+
+# class CategoryView(generics.ListCreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+# class SingleCategoryView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+# class ProductView(generics.ListCreateAPIView):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+
+# class SingleProductView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+
+# class SupplierView(generics.ListCreateAPIView):
+#     queryset = Supplier.objects.all()
+#     serializer_class = SupplierSerializer
+
+# class SingleSupplierView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Supplier.objects.all()
+#     serializer_class = SupplierSerializer
+
+# class EntityView(generics.ListCreateAPIView):
+#     queryset = Entity.objects.all()
+#     serializer_class = EntitySerializer
+
+# class SingleEntityView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Entity.objects.all()
+#     serializer_class = EntitySerializer
+
+
+
+
