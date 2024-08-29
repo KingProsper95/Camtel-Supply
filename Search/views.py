4bd02f78eb983844c196504d596230fa41d0aaf1
KingProsper95 <ajoninajerryprosper@gmail.com>
2024-08-29 11:30:59 +0100

    installing the jwt

--- Search/views.py
+++ Search/views.py
@@ -4,13 +4,20 @@ from django.shortcuts import render
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
 
 from . import client
 # Create your views here.
+
 class SearchListView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
-        query = request.GET.get('q')
-        if not query:
-            return Response('', status=400)
-        results = client.perform_search(query)
-        return Response(results)
+        if request.user.is_authenticated:
+            query = request.GET.get('q')
+            if not query:
+                return Response('', status=400)
+            results = client.perform_search(query)
+            print(request.user.is_authenticated)
+            return Response(results)
+        return Response({"status" : "Can't perform this search you are not authenticated"},
+                        status.HTTP_401_UNAUTHORIZED
+                        )
